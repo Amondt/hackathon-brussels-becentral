17e819ab8e326a9fcb42156ef510e2a3b4c1a620
Matt <meurant.m@gmail.com>
2019-03-31 05:47:04 +0200

    Modification
    mod

--- New test version with pict/script.js
+++ New test version with pict/script.js
@@ -1,5 +1,10 @@
-document.querySelector("#magicButton").addEventListener("click", function(){
-  document.querySelector('#iframeContainer').style.left = '00px';
- document.getElementsByTagName("section").style.opacity = "0.3";
 
-});
+window.onload = () => {
+  document.querySelector("#magicButton").addEventListener("click", function(){
+    document.body.style.overflow = 'hidden'
+    window.scrollTo({top:0, behavior: 'smooth'})
+    document.querySelector('#iframeContainer').style.left = '0px'
+    document.getElementsByTagName("section").style.opacity = "0.3"
+
+  });
+}
